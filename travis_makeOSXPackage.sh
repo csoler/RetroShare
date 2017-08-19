3860a3235f22194ea8aa571acca54e2bf839904d
csoler <csoler@users.sourceforge.net>
2017-08-19 16:02:05 +0200

    attempt to auto-package for OSX using travis

---
+++ travis_makeOSXPackage.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Install the 7z to create dmg archives.
+brew install p7zip
+
+# Package your app
+echo "Packaging YourApp..."
+cd ${project_dir}/build/macOS/clang/x86_64/release/
+
+# Remove build directories that you don't want to deploy
+rm -rf moc
+rm -rf obj
+rm -rf qrc
+
+echo "Creating dmg archive..."
+macdeployqt retroshare.app -qmldir=../../../../../src -dmg
+mv retroshare.dmg "retroshare_${TAG_NAME}.dmg"
+
+# You can use the appdmg command line app to create your dmg file if
+# you want to use a custom background and icon arrangement. I'm still
+# working on this for my apps, myself. If you want to do this, you'll
+# remove the -dmg option above.
+# appdmg json-path YourApp_${TRAVIS_TAG}.dmg
+
+# Copy other project files
+cp "${project_dir}/README.md" "README.md"
+cp "${project_dir}/LICENSE" "LICENSE"
+cp "${project_dir}/Qt License" "Qt License"
